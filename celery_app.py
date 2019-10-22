39840337ed6d74dd1746c55ff506388ba6d00f3a
nining <nining@gstianfu.com>
2019-10-22 10:23:56 +0800

    adjust celery_config

--- celery_app.py
+++ celery_app.py
@@ -3,11 +3,6 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
 
-app = Celery('celery-alpha',
-             include=['tasks.operation'],
-             broker='pyamqp://guest@127.0.0.1//',
-             backend='redis://127.0.0.1')
-
-# app.config_from_object('config.celery_config')
-
+app = Celery('celery_alpha_main')
+app.config_from_object('config.celery_config')
 
--- config/celery_config.py
+++ config/celery_config.py
@@ -1,4 +1,9 @@
 # -*-coding:utf-8 -*-
 
+include = ('tasks.operation', )
+
 broker_url = 'pyamqp://guest@127.0.0.1//'
 result_backend = 'redis://127.0.0.1'
+
+enable_utc = False
+timezone = 'Asia/Shanghai'
