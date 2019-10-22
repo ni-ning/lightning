d74eed54e3f9f46d3e72a2eb970bb6be69940335
nining <nining@gstianfu.com>
2019-10-22 09:51:37 +0800

    add test info

--- celery_app.py
+++ celery_app.py
@@ -3,7 +3,11 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
 
-app = Celery('celery-alpha', include=['tasks.operation'])
-app.config_from_object('config.celery_config')
+app = Celery('celery-alpha',
+             include=['tasks.operation'],
+             broker='pyamqp://guest@127.0.0.1//',
+             backend='redis://127.0.0.1')
+
+# app.config_from_object('config.celery_config')
 
 
