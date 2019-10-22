8832a24d6468b5a67e8d9fae96a56dba1e2b12bd
nining <nining@gstianfu.com>
2019-10-22 10:28:10 +0800

    add test info

--- celery_app.py
+++ celery_app.py
@@ -4,5 +4,5 @@ from __future__ import absolute_import, unicode_literals
 from celery import Celery
 
 app = Celery('celery_alpha_main')
-app.config_from_object('config.celery_config')
+app.config_from_object('config.celery')
 
