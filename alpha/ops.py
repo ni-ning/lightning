9d4822ca87f66be5203f3d4105ae1f73f86086a1
nining <nining@gstianfu.com>
2019-10-23 10:52:02 +0800

    add ops

--- alpha/ops.py
+++ alpha/ops.py
@@ -1,2 +1,17 @@
 # -*-coding:utf-8 -*-
+import datetime
+from celery_app import app
 
+
+def async_calc(x, y):
+    calc.apply_async(args=(x, y), kwds={'verbose': 'Hello world!'})
+
+
+@app.task
+def calc(x, y, verbose=None):
+    print('start calc: %s' % str(datetime.datetime.now()))
+    if verbose:
+        print(verbose)
+    ret = x + y
+    print('%s + %s = %s' % (x, y, ret))
+    return ret
--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,6 @@
 # -*-coding:utf-8 -*-
 
-include = ('tasks.operation', )
+include = ('tasks.operation', 'alpha.ops')
 
 broker_url = 'pyamqp://guest@127.0.0.1//'
 result_backend = 'redis://127.0.0.1'
