b2080f3742aa53808d744b2199daed00a61f6502
nining <nining@gstianfu.com>
2019-10-24 16:33:16 +0800

    add name test

--- gamma/cmd.py
+++ gamma/cmd.py
@@ -2,6 +2,11 @@
 '''
 命令行参数解析模块 argparse，即对sys.args进行解析
 支持自动生成help命令和帮助文档
+
+-h 直接输出帮助文档，无其他流程
+-v 直接输出版本信息，无其他流程
+
+-d 则会进入解析流程 args
 '''
 
 import argparse
@@ -10,11 +15,16 @@ parser = argparse.ArgumentParser(description='This script is just used for test.
 parser.add_argument('-v', '--version', action='version',
                     version='%(prog)s version: 0.0.1', help='show the version')
 
+# 默认(即不指定)为False; -d, --debug 则debug=True
 parser.add_argument('-d', '--debug', action='store_true', default=False,
                     help='show the debug')
 
-args = parser.parse_args()
+# 用单词做参数，默认为必选参数
+parser.add_argument('name')
 
+args = parser.parse_args()
 args = vars(args)
 print(args)
+
+
 
