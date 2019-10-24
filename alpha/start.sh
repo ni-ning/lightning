e33b87e19d30966b5e85bc8d9c76572e6ee5d63f
nining <nining@gstianfu.com>
2019-10-24 15:52:28 +0800

    add test bash

--- alpha/start.sh
+++ alpha/start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin
+#!/usr/bin/env bash
 
 echo "start processing..."
 
@@ -13,3 +13,14 @@ if [[ ! "$?" ]]; then
   sudo docker container rm redis
 fi
 echo "rm docker redis done."
+
+sudo docker run -it -d --name rabbitmq  \
+  -p 5672:5672 \
+  -p 5671:5671 \
+  -p 4369:4369 \
+  -p  25672:25672 \
+  rabbitmq
+echo "succeed to start docker rabbitmq"
+
+sudo docker run -it -d --name redis -p 6379:6379 redis
+echo "succeed to start docker redis"
