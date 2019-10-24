bac3e7e910a5add59bb04ae9e30fe299c5ccccfc
nining <nining@gstianfu.com>
2019-10-24 15:37:02 +0800

    add test info

--- alpha/start.sh
+++ alpha/start.sh
@@ -2,13 +2,13 @@
 
 echo "start processing..."
 
-sudo docker container stop rabbitmq
+sudo docker container stop rabbitmq &> /dev/null
 if [[ ! "$?" ]]; then
     sudo docker container rm rabbitmq
 fi
 echo "rm docker rabbitmq done."
 
-sudo docker container stop redis
+sudo docker container stop redis &> /dev/null
 if [[ ! "$?" ]]; then
   sudo docker container rm redis
 fi
