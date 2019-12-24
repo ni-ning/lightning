69ade2ba3612d4a6c78ea4953b7bee49edaa49cc
nining <nining@gstianfu.com>
2019-12-24 11:31:51 +0800

    add hello test

--- alpha/config/celery.py
+++ alpha/config/celery.py
@@ -1,6 +1,8 @@
 # -*-coding:utf-8 -*-
 
-include = ('alpha.tasks.basic_task', 'alpha.ops')
+include = ('alpha.tasks.basic_task',
+           'alpha.tasks.bound_task',
+           'alpha.ops')
 
 broker_url = 'pyamqp://guest@127.0.0.1//'
 result_backend = 'redis://127.0.0.1'
--- alpha/tasks/bound_task.py
+++ alpha/tasks/bound_task.py
@@ -1,12 +1,23 @@
 # -*- coding:utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-import logging
+import time
 from celery_app import app
 
 
+# Celery support catching all states changes by setting on_message callback.
 @app.task(bind=True)
-def add(self, x, y):
-    logging.info(self.request.id)
-    return x + y
+def hello(self, x, y):
+    time.sleep(5)
+    self.update_state(state='PROGRESS', meta={'progress': 50})
+    time.sleep(5)
+    self.update_state(state='PROGRESS', meta={'progress': 90})
+    time.sleep(3)
+    return 'hello world: %s' % (x + y)
+
+
+def on_raw_message(body):
+    print(body)
+
+
 
