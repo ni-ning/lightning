bad4881c34186211877c148a15066d9d5f665984
nining <nining@gstianfu.com>
2019-12-24 11:45:14 +0800

    add test info

--- alpha/tasks/bound_task.py
+++ alpha/tasks/bound_task.py
@@ -6,14 +6,24 @@ from celery_app import app
 
 
 # Celery support catching all states changes by setting on_message callback.
+# @app.task(bind=True)
+# def hello(self, x, y):
+#     time.sleep(5)
+#     self.update_state(state='PROGRESS', meta={'progress': 50})
+#     time.sleep(5)
+#     self.update_state(state='PROGRESS', meta={'progress': 90})
+#     time.sleep(3)
+#     return 'hello world: %i' % (x + y)
+
+
 @app.task(bind=True)
-def hello(self, x, y):
-    time.sleep(5)
-    self.update_state(state='PROGRESS', meta={'progress': 50})
-    time.sleep(5)
-    self.update_state(state='PROGRESS', meta={'progress': 90})
-    time.sleep(3)
-    return 'hello world: %i' % (x + y)
+def hello(self, a, b):
+    time.sleep(1)
+    self.update_state(state="PROGRESS", meta={'progress': 50})
+    time.sleep(1)
+    self.update_state(state="PROGRESS", meta={'progress': 90})
+    time.sleep(1)
+    return 'hello world: %i' % (a+b)
 
 
 def on_raw_message(body):
