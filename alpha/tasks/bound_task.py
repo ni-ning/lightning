bba19aae0275656231eb21982c834d8342fd42cd
nining <nining@gstianfu.com>
2019-12-24 11:50:50 +0800

    add print info

--- alpha/tasks/bound_task.py
+++ alpha/tasks/bound_task.py
@@ -18,11 +18,8 @@ from celery_app import app
 
 @app.task(bind=True)
 def hello(self, a, b):
-    time.sleep(1)
-    self.update_state(state="PROGRESS", meta={'progress': 50})
-    time.sleep(1)
-    self.update_state(state="PROGRESS", meta={'progress': 90})
-    time.sleep(1)
+    print(self)
+    print(a, b)
     return 'hello world: %i' % (a+b)
 
 
