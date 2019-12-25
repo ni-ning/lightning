a682e40da80bcacfc416d43ccf5e17db41f5018f
nining <nining@gstianfu.com>
2019-12-25 14:25:32 +0800

    pip install librabbitmq

--- alpha/config/celery.py
+++ alpha/config/celery.py
@@ -8,8 +8,8 @@ include = ('alpha.tasks.basic_task',
            'alpha.tasks.schedule',
            'alpha.ops')
 
-# broker_url = 'pyamqp://guest@127.0.0.1//'
-broker_url = 'librabbitmq://guest@127.0.0.1//'
+broker_url = 'pyamqp://guest@127.0.0.1//'
+# broker_url = 'librabbitmq://guest@127.0.0.1//'
 result_backend = 'redis://127.0.0.1'
 
 enable_utc = False
