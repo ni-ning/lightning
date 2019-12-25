3bb30bc746cb65e753845f761f3f804e24541aba
nining <nining@gstianfu.com>
2019-12-25 16:54:05 +0800

    add logger

--- celery_app.py
+++ celery_app.py
@@ -1,8 +1,11 @@
 # -*-coding:utf-8 -*-
-
 from __future__ import absolute_import, unicode_literals
+
 from celery import Celery
 from celery.signals import after_task_publish, celeryd_after_setup
+from celery.utils.log import get_logger
+
+logger = get_logger(__name__)
 
 app = Celery('celery_alpha_main')
 app.config_from_object('alpha.config.celery')
@@ -12,9 +15,9 @@ app.config_from_object('alpha.config.celery')
 def task_sent_handler(sender=None, headers=None, body=None, **kw):
 
     info = headers if 'task' in headers else body
-    print('after_task_publish for task id {info[id]}'.format(info=info, ))
+    logger.info('after_task_publish for task id {info[id]}'.format(info=info, ))
 
 
 @celeryd_after_setup.connect
 def setup_direct_queue(*arg, **kw):
-    print('setup_direct_queue arg: %s, kw: %s' % (arg, kw))
+    logger.info('setup_direct_queue arg: %s, kw: %s' % (arg, kw))
