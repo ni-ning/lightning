5b71164986998865689392defbc9c233953e5c10
nining <nining@gstianfu.com>
2019-12-25 10:24:36 +0800

    modify celery config

--- alpha/config/celery.py
+++ alpha/config/celery.py
@@ -1,5 +1,6 @@
 # -*-coding:utf-8 -*-
 
+from kombu import Queue, Exchange
 from celery.schedules import crontab
 
 include = ('alpha.tasks.basic_task',
@@ -15,8 +16,31 @@ timezone = 'Asia/Shanghai'
 
 worker_redirect_stdouts_level = 'DEBUG'
 
+# If enabled(default), any queues specified that aren't defined in task_queues
+# will be automatically created.
+task_create_missing_queues = True
+
+# a list of Queue instances
+task_queues = (
+    Queue('tianfu-gbm-celery-task-main',
+          exchange=Exchange('tianfu-gbm-celery-task-main'),
+          routing_key='tianfu-gbm-celery-task-main',
+          type='direct'),
+)
+
 # 默认队列名 celery -> default
-task_default_queue = 'default'
+task_default_queue = 'tianfu-gbm-celery-task-main'
+task_default_exchange = 'tianfu-gbm-celery-task-main'
+task_default_routing_key = 'tianfu-gbm-celery-task-main'
+task_default_exchange_type = 'direct'
+
+# 路由配置，and override this using the routing_key argument to Task.apply_async()
+# task_routes = {
+#         'feeds.tasks.import_feed': {
+#             'queue': 'feed_tasks',
+#             'routing_key': 'feed.import',
+#         },
+# }
 
 # 周期性任务配置
 beat_schedule = {
